afdc5e2ecbeae7f16b6f2c9bac28eef18af907b3
umaedi <umaedi.kh.99@gmail.com>
2025-06-25 17:30:52 +0700

    fix: update InstagramController to filter and limit fetched posts

--- app/Http/Controllers/Api/InstagramController.php
+++ app/Http/Controllers/Api/InstagramController.php
@@ -18,9 +18,17 @@ class InstagramController extends Controller
         try {
            $response = Http::get('http://192.168.11.2:9913/api/instagram');
             $responseData = $response->json()['metadata'];
+
+            // Filter hanya media_type yang bukan VIDEO
+            $filtered_posts = array_filter($responseData, function ($post) {
+                return $post['media_type'] == 'CAROUSEL_ALBUM';
+            });
+
+            // Ambil hanya 4 data pertama dari hasil filter
+            $post_instagram = array_slice(array_values($filtered_posts), 0, 6);
             return response()->json([
                 'message' => 'Instagram posts fetched successfully',
-                'metadata' => $responseData
+                'metadata' => $post_instagram
             ]);
         } catch (\Throwable $th) {
             return response()->json([
